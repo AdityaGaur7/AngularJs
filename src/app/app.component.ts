20e925879efe485e946bf9efbcb750bfba516e87
AdityaGaur7 <adgaur027@gmail.com>
2024-07-13 21:41:56 +0530

    two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './user-auth/login/login.component';
 import { ChildComponent } from './child/child.component';
+import {FormsModule}from "@angular/forms"
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/user-auth/login/login.component.ts
+++ src/app/user-auth/login/login.component.ts
@@ -1,27 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 
 export class LoginComponent {
-  value = "";
-   userdata:any =[];
-   submit(val:any){
-    this.value=val;
-this.userdata.push({id:this.userdata.length,name:this.value})
-
-   }
-   remove(id:any){
-      // this.userdata.splice(id,1)
-      this.userdata = this.userdata.filter((item:any) => item.id !== id);
-
-
-   }
+name:any;
   
 }
 
